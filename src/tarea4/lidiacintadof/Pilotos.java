87e528bb7772f904afd7ae6d22bc9a9e50b0cb8d
lidia <lidiacintadog@gmail.com>
2019-02-13 13:08:49 +0100

    Añade atributos en clase modelo

---
+++ src/tarea4/lidiacintadof/Pilotos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4.lidiacintadof;
+
+/**
+ *
+ * @author bicho
+ */
+public class Pilotos {
+    String nombre;
+    String ape;
+    int edad;
+}
